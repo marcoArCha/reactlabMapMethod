59173875aab2e9525332f819d960071a34311b43
Marco Arias <marcoarccha@hotmail.com>
2024-05-16 10:16:14 -0600

    Lab completed

--- src/DessertsList.js
+++ src/DessertsList.js
@@ -1,6 +1,20 @@
 function DessertsList(props) {
   // Implement the component here.
-  return null;
+  const fitDesserts = props.data
+  .filter(objecto => objecto.calories <500)
+  .sort((a, b) => a.calories - b.calories)
+
+  return(
+    <ul>
+      {
+        fitDesserts.map((postre, index) =>(
+          <li key={index}>
+            {postre.name} - {postre.calories} calories
+          </li>
+        )
+      )}
+    </ul>
+  );  
 }
 
 export default DessertsList;
